292453add5d37d88b2e68488bfd9cd786a65d7ec
Johnny Moreira <jmoreira@engebras.com.br>
2024-07-18 08:36:10 -0300

    fix all

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/johnny/Desktop/egb/python/tesseract/venv"
+set -gx VIRTUAL_ENV "/home/johnny/Desktop/egb/python/defImage/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
